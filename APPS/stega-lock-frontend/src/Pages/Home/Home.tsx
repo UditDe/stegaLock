e2b2324c32211f3b0471fa9ff82749c4d758f1ff
Udit Kanti De <uditde.com@gmail.com>
2026-05-09 16:26:13 +0530

    logo added

--- APPS/stega-lock-frontend/src/Pages/Home/Home.tsx
+++ APPS/stega-lock-frontend/src/Pages/Home/Home.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Home.scss"
 import ProfileSection from './components/ProfileSection'
 import PasswordData from './components/PasswordDataTable'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const Home: React.FC = () => {
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  useEffect(() => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [isAuthenticated, loginWithRedirect]);
   return (
     <div className='homewrapper'>
       <ProfileSection />
